ef15f8bcb008ddcb9c28a8970091279f13fd9658
miha11-hash <d_mihaela@msn.com>
2023-08-19 17:41:03 +0300

    verify token

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,8 @@
 
 const BadRequest=require("../errors/bad-request.js");
+const CustomErrors= require("../errors/custom-errors.js");
+const jwt = require("jsonwebtoken");
+
 
 
 const login = async (req, res) => {
@@ -11,15 +14,70 @@ const login = async (req, res) => {
     if(!username || !password) {
         throw new BadRequest("please provide username or password", 400);
     }
+    //demo, normally provided by DB
+    const id=new Date().getDate();
 
-    res.send("fake login register/signup")
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: "3d"});
+
+    res.status(200).json({msg: "user created", token})
 }
 
-const dashboard = async (req, res) => {
-const randomNumber = Math.floor(Math.random()*100);
-console.log(randomNumber);
-return res.status(200).json({msg: "hello, john doe", secret: `your secret code is ${randomNumber}`})
+async function dashboard (req, res) {
+const authHeader=req.headers.authorization;
+
+if(!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw BadRequest("no token provided", 401)
 }
 
-module.exports = {login, dashboard};
+const token=authHeader.split(" ")[1];
+console.log(token);
+const luckyNumber = Math.floor(Math.random() * 100);
+try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    return res.status(200).json({msg: `hello, ${decoded.username}`, secret: `your luckyNumber is ${luckyNumber}`})
+} catch (error) {
+   throw new CustomErrors("not authorized to use this route", 401) 
+}
+
+}
+
+
+
+module.exports={login, dashboard};
+
+
+// const dashboard = async (req, res) => {
+
+// const authHeaders = req.headers.authorization;
+// if(!authHeaders || !authHeaders.startsWith("Bearer ")) {
+//     throw new BadRequest("No token provided", 401);
+// }
+
+// const token= authHeaders.split(",")[1];
+// try {
+//     //console.log(randomNumber);
+//     console.log(req.headers);
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     console.log(decoded);
+//     const randomNumber = Math.floor(Math.random() * 100);
+//     return res
+//       .status(200)
+//       .json({
+//         msg: "hello, john doe",
+//         secret: `your secret code is ${randomNumber}`,
+//       });
+// } catch (error) {
+//     throw new CustomErrors("the route you access does not exist", 401)
+// }
+// }
+
+
+
+
+
+
+
+
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,7 +3,7 @@ const {StatusCodes}=require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
 if(err instanceof CustomErrors) {
-    return res.status(err.statusCodes).json({msg: err.message})
+    return res.status(err.statusCode).json({msg: err.message})
 }
 return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Something goes wrong, try again later");
 }
